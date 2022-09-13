37ab0445763b77f9b0f5e9261e13517db1389efc
Anton Kopchak <anton.kopchak@gmail.com>
2022-09-13 21:54:40 +0300

    added stack

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Stack from "./stack.js";
+
+const stack = new Stack()
+
+stack.isEmpty()
+
+stack.push(100)
+stack.push(200)
+
+stack.peek()
+
+stack.push(300)
+
+console.log(stack.print())
+
+stack.pop()
+stack.pop()
+
+stack.clear()
+
+console.log(stack.print())
+
+stack.size()
+
+stack.isEmpty()
